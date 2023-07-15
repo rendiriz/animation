ab09270469a0337f901bcb4b625480c0e039e1a0
Rendi Rizki <rendirizkir@gmail.com>
2023-07-15 16:24:09 +0700

    fix: list home

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,33 +1,42 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import { Card, CardContent } from '@/components/ui/card';
+
+const example = [
+  {
+    id: '1de67927-69e4-406c-abc7-919071c2ebb1',
+    name: 'Logo Marquee',
+  },
+  {
+    id: '235ebc7f-6b58-42dc-aaab-00fba3d9a363',
+    name: 'Scroll Stagger Fade In Out',
+  },
+  {
+    id: 'e9d1419a-03c9-41c6-9355-fd3db6350a54',
+    name: 'Scroll Flip Image',
+  },
+];
 
 export default function HomePage() {
   return (
     <div className="w-full mx-auto max-w-4xl py-10">
       <div className="grid grid-cols-2 gap-4">
-        <Link href="/logo-marquee">
-          <Card>
-            <CardContent className="!p-4">
-              <p>Logo Marquee</p>
-            </CardContent>
-          </Card>
-        </Link>
-
-        <Link href="/scroll-stagger-fade-in-out">
-          <Card>
-            <CardContent className="!p-4">
-              <p>Scroll Stagger Fade In Out</p>
-            </CardContent>
-          </Card>
-        </Link>
-
-        <Link href="/scroll-flip-image">
-          <Card>
-            <CardContent className="!p-4">
-              <p>Scroll Flip Image</p>
-            </CardContent>
-          </Card>
-        </Link>
+        {example.map((item, idx) => (
+          <Link key={idx} href={`/${item.id}`}>
+            <Card>
+              <CardContent className="!p-4">
+                <Image
+                  src={`/animation/${item.id}.png`}
+                  alt={item.name}
+                  width="0"
+                  height="0"
+                  sizes="100vw"
+                  className="w-full h-auto rounded-2xl"
+                />
+              </CardContent>
+            </Card>
+          </Link>
+        ))}
       </div>
     </div>
   );
